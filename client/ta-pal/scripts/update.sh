0b437f95e30dd29b2e0ddc2273d38178b83e4b95
Patrick Old <patold@Patricks-MBP.domain>
2018-12-02 12:38:23 -0800

    front end working on server

--- client/ta-pal/scripts/update.sh
+++ client/ta-pal/scripts/update.sh
@@ -2,4 +2,6 @@ docker rm -f client
 docker pull info441tapal/client
 
 docker run -d --name client \
+-p 80:80 \
+-p 443:443 \
 info441tapal/client
--- client/ta-pal/src/Login.js
+++ client/ta-pal/src/Login.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import './Login.css'
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="Login">
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Login">
+                <h2>
+                    Welcome to the Office Hour Manager!
+                </h2>
+                <button id="student-button">
+                    Student View
+                </button>
+                <button id="ta-button">
+                    Teaching Assistant View
+                </button>
+                <footer>
+                    Created by Ben, Godwin, and Patrick
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default Login;
